83bf362d7e61b5880636bbe5f092bd94901b818e
Charlie Schlinkert <schlinkertc@gmail.com>
2019-12-12 14:37:05 -0500

    Thursday afternoon

--- edgar_datafied.py
+++ edgar_datafied.py
@@ -5,17 +5,26 @@ import json
 
 # datafied.api.edgar-online.com/{Version}/{Endpoints}{Format}?{Parameters}appkey={API Key}
 
+def get_symbol(companyname):
+    request = requests.get(f'https://datafied.api.edgar-online.com/v2/companies?companynames={companyname}&appkey={config.edgar_apiKey}')
+    response = request.json()
+    symbol = ({x['field']:x['value']
+               for x in response
+               ['result']['rows'][0]['values']}
+              ['primarysymbol'])
+    return symbol
+
 def corefinancials(primary_symbol):
-    request = requests.get(f"""https://datafied.api.edgar-online.com/v2/corefinancials/ann?primarysymbols={primary_symbol}&numperiods=1&appkey={APPKEY}""")
+    request = requests.get(f"""https://datafied.api.edgar-online.com/v2/corefinancials/ann?primarysymbols={primary_symbol}&numperiods=1&appkey={config.edgar_apiKey}""")
     return request.json()
 
-def get_coreFinancials(symbols):
+def get_coreFinancials(company_names):
     dfs = []
-    for symbol in symbols:
+    for name in company_names:
         try:
-            contents = edgar_datafied.corefinancials(symbol)
+            contents = corefinancials(get_symbol(name))
             financials = {x['field']:x['value'] for x in contents['result']['rows'][0]['values']}
-            temp_df = pd.DataFrame(financials,index=[symbol])
+            temp_df = pd.DataFrame(financials,index=[financials['cik']])
             dfs.append(temp_df)
         except:
             continue
